dfa3dbc72a93868bca54165e2d06ebde06b1c4ad
NicDup04 <nicoleen.duplessis04@gmail.com>
2026-02-02 00:02:04 +0200

    Update app_profiler.py

--- app_profiler.py
+++ app_profiler.py
@@ -20,7 +20,7 @@ st.image(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Laser_Towards_Milky_Ways_Centre.jpg/1280px-Laser_Towards_Milky_Ways_Centre.jpg",
     caption="Wikipedia"
 )
-
+"""
 # Add a section for publications
 st.header("Publications")
 uploaded_file = st.file_uploader("Upload a CSV of Publications", type="csv")
@@ -113,8 +113,9 @@ elif data_option == "Weather Data":
     ]
     st.write(f"Filtered Results for Temperature {temp_filter} and Humidity {humidity_filter}:")
     st.dataframe(filtered_weather)
-
+"""
 # Add a contact section
 st.header("Contact Information")
 email = "jane.doe@example.com"
+
 st.write(f"You can reach {name} at {email}.")
